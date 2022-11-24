a3befb0a3b24c53a09d2e4ed911eabba1f99697a
Suneel Kumar <118738840+Suneel3335@users.noreply.github.com>
2022-11-24 00:41:39 -0800

    Add files via upload

---
+++ shark5.java
@@ -0,0 +1,13 @@
+public class shark5 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int sum = 0, avg;
+        sum = a + b + c;
+        avg = sum / 3;
+        System.out.println("Sum of three numbers is= " + sum);
+        System.out.println("Avg of the numbers is= " + avg);
+    }
+
+}
---
+++ shark7.java
@@ -0,0 +1,9 @@
+public class shark7 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 11; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
