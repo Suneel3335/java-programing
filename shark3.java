350d3dcd4c05848355b669f0b822c2e348d43920
Suneel Kumar <118738840+Suneel3335@users.noreply.github.com>
2022-11-24 00:38:42 -0800

    Add files via upload

---
+++ shark3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class shark3 {
+    public static void main(String args[]) {
+        int len;
+
+        String sentence;
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter a sennetce");
+        sentence = obj.nextLine();
+        len = sentence.length();
+        for (int i = 0; i > len; i++) {
+
+        }
+
+    }
+
+}
